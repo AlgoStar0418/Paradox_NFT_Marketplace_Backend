253909402148337c168bd1a45935232692541589
AlgoStar0418 <bs07120418@gmail.com>
2023-06-29 12:08:56 +0900

    update

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   // mongoURI: "mongodb://localhost:27017/db_paradox_nft_marketplace",
   mongoURI:
-    "mongodb+srv://devleopard0330:Z2oM8zKriemftDSY@nftmarketplace.iuovzmf.mongodb.net/",
+    "mongodb+srv://devleopard0330:Z2oM8zKriemftDSY@nftmarketplace.iuovzmf.mongodb.net/db_paradox_nft_marketplace",
   secretOrKey: "secret",
   port: 5000,
 };
